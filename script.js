2bc1eeaebae48aedadcc436b5ec03f523c998cd2
alejo7762 <alejito7762@gmail.com>
2025-08-18 22:26:31 -0500

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,14 @@ window.onload = function() {
     console.log("Página de documentación ERP cargada con paleta Beige y Café.");
 };
 
+window.onload = function() {
+    const header = document.querySelector("header");
+    const main = document.querySelector("main");
+
+    let alturaHeader = header.offsetHeight;
+    main.style.paddingTop = alturaHeader + "px";
+};
+
 window.addEventListener("scroll", function() {
     let header = document.querySelector("header");
     header.classList.toggle("scrolled", window.scrollY > 0);
