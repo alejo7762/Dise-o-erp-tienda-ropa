27b506b724b3787f0ccb86b185662a9d2d74e970
alejo7762 <alejito7762@gmail.com>
2025-08-18 21:51:39 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function mostrarAlerta() {
+    alert("Aquí se mostrará el diagrama de la arquitectura del ERP.");
+}
+
+// Ejemplo: mostrar en consola cuando se carga la página
+window.onload = function() {
+    console.log("Página de documentación ERP cargada correctamente.");
+};
